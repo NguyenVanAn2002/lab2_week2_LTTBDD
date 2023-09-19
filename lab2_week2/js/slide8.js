97ea8867a893e9441d8d4bb5812291fa7b36c1d4
NguyenVanAn2002 <98084517+NguyenVanAn2002@users.noreply.github.com>
2023-09-19 14:39:55 +0700

    Add files via upload

---
+++ lab2_week2/js/slide8.js
@@ -0,0 +1,12 @@
+var url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m";
+var xhr = new XMLHttpRequest()
+xhr.open('GET', url + '/1', true)
+xhr.onload = function() {
+    var users = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+        console.table(users);
+    } else {
+        console.error(users);
+    }
+}
+xhr.send(null);
